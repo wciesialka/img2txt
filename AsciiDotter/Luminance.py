e43be58a26789bab680c2b14366ac0ed4f0a21f1
Willow Ciesialka <wciesialka@gmail.com>
2021-11-17 22:25:14 -0500

    Due to the way Enum and Classes behave in Python, any function that you give an Enum will act like a method. To remedy this, we have to wrap our functions. Added a wrapper and decorator for this purpose.

--- AsciiDotter/Luminance.py
+++ AsciiDotter/Luminance.py
@@ -1,6 +1,24 @@
 from enum import Enum
 from typing import Callable
 
+
+# Due to the way that Python handles Enums, we have to wrap our functions in a class to
+# be able to put them in an Enum. __EnumerableFunction is the wrapper class, and it's __call__ is set to
+# mimic it's inner function. The __enumerable_function decorator will make any function
+# enumerable by returning the function wrapped in our class.
+
+class __EnumerableFunction:
+    '''A simple wrapper class for any function.'''
+    def __init__(self,func):
+        self.__func = func
+    
+    def __call__(self,*args,**kwargs):
+        return self.__func(*args,**kwargs)
+
+def __enumerable_function(func):
+    return __EnumerableFunction(func)
+
+
 def __map(value:float,lower_bound:float,upper_bound:float) -> float:
     '''Map a value that is in the range of [lower bound, upper bound] to [0.0, 1.0].\n
     A value between [0.0, 1.0] is not guaranteed (for example, if value is less than lower bound or greater than upper bound.)
@@ -17,6 +35,7 @@ def __map(value:float,lower_bound:float,upper_bound:float) -> float:
     
     return (value - lower_bound) * 1.0 / (upper_bound - lower_bound)
 
+@__enumerable_function
 def calculate_average(r:int, g:int, b:int) -> float:
     '''Get the luminance of a color in RGB space using the average of the three colors.
     
@@ -33,6 +52,7 @@ def calculate_average(r:int, g:int, b:int) -> float:
 
     return __map(avg,0,255)
 
+@__enumerable_function
 def calculate_luminance(r:int, g:int, b:int) -> float:
     '''Get the luminance of a color in RGB space using the relative luminance formula.
     
@@ -54,6 +74,7 @@ def calculate_luminance(r:int, g:int, b:int) -> float:
 
     return __map(y,0,196964.69911399897)
 
+@__enumerable_function
 def calculate_value(r:int, g:int, b:int):
     '''Get the luminance of a color in RGB space by calculating it's value.
     
@@ -69,6 +90,7 @@ def calculate_value(r:int, g:int, b:int):
 
     return max(r/255,g/255,b/255)
 
+@__enumerable_function
 def calculate_weighted(r:int, g:int, b:int):
     '''Get the luminance of a color in RGB space by calculating it's weighted luminance. Similar to relative luminance.
     

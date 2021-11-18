66e3be707871e4d07f07cdb262bd7b19c81ad743
Willow Ciesialka <wciesialka@gmail.com>
2021-11-18 01:34:18 -0500

    Refactor of setup.py. New score: 10.0

--- setup.py
+++ setup.py
@@ -1,21 +1,21 @@
 #!/bin/env python3
-
-from setuptools import setup, find_packages
+'''Setup script.'''
 from pathlib import Path
+from setuptools import setup, find_packages
 
-this_directory = Path(__file__).parent
+THIS_DIRECTORY = Path(__file__).parent
 
-requirements = (this_directory / "requirements.txt").read_text().split('\n')[:-1]
-long_description = (this_directory / "README.md").read_text()
+REQUIREMENTS = (THIS_DIRECTORY / "requirements.txt").read_text().split('\n')[:-1]
+LONG_DESCRIPTION = (THIS_DIRECTORY / "README.md").read_text()
 
-content = {
+CONTENT = {
     "name": "AsciiDotter",
     "version": "2.1.0",
     "author": "Willow Ciesialka",
     "author_email": "wciesialka@gmail.com",
     "url": "https://github.com/wciesialka/ascii-dotter",
     "description": "CLI tool for turning images into ascii art.",
-    "long_description": long_description,
+    "long_description": LONG_DESCRIPTION,
     "long_description_content_type": "text/markdown",
     "license": "GPL-3.0",
     "packages": find_packages(where="src"),
@@ -25,16 +25,16 @@ content = {
         ]
     },
     "classifiers": [
-            "Programming Language :: Python :: 3",
-            "License :: OSI Approved :: GNU Affero General Public License v3",
-            "Topic :: Artistic Software",
-            "Operating System :: OS Independent"
+        "License :: OSI Approved :: GNU Affero General Public License v3",
+        "Programming Language :: Python :: 3",
+        "Topic :: Artistic Software",
+        "Operating System :: OS Independent"
     ],
     "keywords": "python image ascii art",
     "package_dir": {"": "src"},
-    "install_requires": requirements,
+    "install_requires": REQUIREMENTS,
     "zip_safe": False,
     "python_requires": ">=3.8.10"
 }
 
-setup(**content)
+setup(**CONTENT)

9feec00e42b3866be3dd0c623332780e2f02eec6
Willow Ciesialka <wciesialka@gmail.com>
2024-01-06 21:02:05 -0500

    Updated methods module to avoid namespace poisoning.

--- src/img2txt/methods/__init__.py
+++ src/img2txt/methods/__init__.py
@@ -3,16 +3,16 @@
 :author: Willow Ciesialka
 '''
 
-import img2txt.methods.threshold as threshold_methods
-import img2txt.methods.colors as color_methods
+import img2txt.methods.threshold
+import img2txt.methods.colors
 
 THRESHOLD_METHODS = {}
-THRESHOLD_METHODS["luminance"] = threshold_methods.luminance_method
-THRESHOLD_METHODS["lightness"] = threshold_methods.lightness_method
+THRESHOLD_METHODS["luminance"] = img2txt.methods.threshold.luminance_method
+THRESHOLD_METHODS["lightness"] = img2txt.methods.threshold.lightness_method
 
 COLOR_METHODS = {}
-COLOR_METHODS["none"] = color_methods.PlaintextVisitor
-COLOR_METHODS["4bitansi"] = color_methods.FourBitAnsiVisitor
-COLOR_METHODS["8bitansi"] = color_methods.EightBitAnsiVisitor
-COLOR_METHODS["truecoloransi"] = color_methods.TrueColorAnsiVisitor
-COLOR_METHODS["html"] = color_methods.HTMLVisitor
+COLOR_METHODS["none"] = img2txt.methods.colors.PlaintextVisitor
+COLOR_METHODS["4bitansi"] = img2txt.methods.colors.FourBitAnsiVisitor
+COLOR_METHODS["8bitansi"] = img2txt.methods.colors.EightBitAnsiVisitor
+COLOR_METHODS["truecoloransi"] = img2txt.methods.colors.TrueColorAnsiVisitor
+COLOR_METHODS["html"] = img2txt.methods.colors.HTMLVisitor

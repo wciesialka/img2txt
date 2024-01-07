c6e2c1f1c50d668e299045ded8a7f4d6a1cab546
Willow Ciesialka <wciesialka@gmail.com>
2024-01-06 20:48:46 -0500

    Added method dictionaries.

--- src/img2txt/methods/__init__.py
+++ src/img2txt/methods/__init__.py
@@ -3,10 +3,16 @@
 :author: Willow Ciesialka
 '''
 
-import img2txt.methods.threshold as threshold
-import img2txt.methods.colors as colors
+import img2txt.methods.threshold as threshold_methods
+import img2txt.methods.colors as color_methods
 
 THRESHOLD_METHODS = {}
-THRESHOLD_METHODS["luminance_filter"] = threshold.luminance_filter
+THRESHOLD_METHODS["luminance"] = threshold_methods.luminance_method
+THRESHOLD_METHODS["lightness"] = threshold_methods.lightness_method
 
-COLOR_METHODS = {"none": None}
+COLOR_METHODS = {}
+COLOR_METHODS["none"] = color_methods.PlaintextVisitor
+COLOR_METHODS["4bitansi"] = color_methods.FourBitAnsiVisitor
+COLOR_METHODS["8bitansi"] = color_methods.EightBitAnsiVisitor
+COLOR_METHODS["truecoloransi"] = color_methods.TrueColorAnsiVisitor
+COLOR_METHODS["html"] = color_methods.HTMLVisitor
